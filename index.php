33c09d78e0bd4b0e13b82ed24c68eb13e01eac70
Hoang Anh <hoanganhtran1998@gmail.com>
2019-03-25 17:32:52 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Flight Booking</title>
+        <link rel="stylesheet" href="CSS/home.css"/>
+    </head>
+    <body>
+        <div class="body">
+            <div class="nav-bar">
+                <div>
+                    <img width="45" height="45" src="images/ticket.png"/>
+                </div>
+                <div>
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
