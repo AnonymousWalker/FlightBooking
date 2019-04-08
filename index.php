a95b15798201e419ddc86ae5e778d4d81f823d39
Hoang Anh <hoanganhtran1998@gmail.com>
2019-04-08 15:37:42 -0500

    refactor modal into separate file

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['logged'])) {
             $controller->getFlights();
             ?>
         </div>
-        <?php include_once "_modalPopup.php" ?>
+        <?php include_once "_modalPopup.html" ?>
 
 
     </body>
